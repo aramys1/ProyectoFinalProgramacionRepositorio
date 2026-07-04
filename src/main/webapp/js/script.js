e65891feae9a17c2f16f60f4bbef5f250f4e8249
Kaxandra <alissonanais.aa16@gmail.com>
2026-07-04 18:07:44 -0500

    Guardando mis cambios locales antes de actualizar

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     initPosterFileName();
     initInventoryTemplate();
     initSynopsisInventoryTemplate();
+    initEmployeeInventoryFilter();
 });
 
 function initAuthForms() {
